4ee20a0c277d23f08c577bebbe6c6bf7803ef4db
iliay038 <iliay038@tlvm-4-4-3.ad.liu.se>
2021-12-10 16:03:33 +0100

    sending signature to peers, verification in progress

--- ddagb/db.py
+++ ddagb/db.py
@@ -2,6 +2,10 @@ from os import error
 import rocksdb
 import json
 import hashlib
+from Crypto.PublicKey import RSA
+from Crypto.Signature import pkcs1_15
+from Crypto.Hash import SHA256
+import binascii
 def put_db(key, value, address):
     db = rocksdb.DB(address, rocksdb.Options(create_if_missing=True))
     db.put(bytes(key), bytes(value), address)
@@ -10,7 +14,7 @@ def get_db(key, address):
     db = rocksdb.DB(address, rocksdb.Options(create_if_missing=True))
     return db.get(bytes(key), address)
 
-def append_commit(file,body,address, ref, id):
+def append_commit(file,body,address, ref, pub_key, priv_key):
     body=body.encode()
     type, empty , data = body.partition(b'\x00')
     data=data.decode()
@@ -37,15 +41,76 @@ def append_commit(file,body,address, ref, id):
             "tree": t_data,
             "parents": parent_list,
             "message": msg,
-            "user" : id,
-            "branch" : ref,
             "commit" : file,
+            "branch" : ref,
+            "user" : pub_key,
             "dag_parents": dag_parents
         })
-        dag_hash= hashlib.sha256(tot.encode()).hexdigest()
+        dag_h= SHA256.new(tot.encode())
+        dag_hash=dag_h.hexdigest()
+        print("about to sign")
+        try:
+            signature = pkcs1_15.new(priv_key).sign(dag_h)
+            print("just signed")
+        except:
+            print("failed to sign")
         #find new way to generate key, hash rest of block also to generate key, might cause issues with parent hash,
         #since git commit hash will then be different from DAG hash.
         put_db(dag_hash.encode(),tot.encode(),address.encode())
+
+        return signature
+    return
+
+def get_block(file,body,address, ref,pu_key,pub_key, signature):
+    body=body.encode()
+    type, empty , data = body.partition(b'\x00')
+    data=data.decode()
+    data_list=data.split("\n")
+    type=type.decode()
+    if type== "commit":
+        t_data= data_list[0].split(" ",1)[1]
+        parent_list=[]
+        i = 1
+        while(i < 3):
+            try:
+                parent, p_data= data_list[i].split(" ",1)
+            except:
+                break
+            if "parent" in parent:
+                parent_list.append(p_data)
+            else:
+                break
+            i+=1
+
+        msg = ''.join(data_list[i:])
+        dag_parents= find_dag_parents(parent_list, address)
+        #p_key= pub_key.export_key().decode()
+        tot=json.dumps({
+            "tree": t_data,
+            "parents": parent_list,
+            "message": msg,
+            "commit" : file,
+            "branch" : ref,
+            "user" : pub_key,
+            "dag_parents": dag_parents
+        })
+        dag_h= SHA256.new(tot.encode())
+        dag_hash=dag_h.hexdigest()
+        sig= binascii.unhexlify(signature)
+
+        try:
+            pkcs1_15.new(pu_key).verify(dag_h, sig)
+            print ("The signature is valid.")
+            put_db(dag_hash.encode(),tot.encode(),address.encode())
+        except (ValueError, TypeError):
+            print ("The signature is not valid.")
+
+        #find new way to generate key, hash rest of block also to generate key, might cause issues with parent hash,
+        #since git commit hash will then be different from DAG hash.
+
+
+
+
 
 
 def find_dag_parents(parent_list, address):
--- ddagb/server.py
+++ ddagb/server.py
@@ -7,8 +7,14 @@ import json
 import db
 import os
 import sec
+import hashlib
+import binascii
 from Crypto.PublicKey import RSA
 from base64 import b64encode
+from Crypto.Signature import pkcs1_15
+from Crypto.Hash import SHA256
+import copy
+
 port = random.randint(1000,5000)
 peers = []
 my_path = f"ws://localhost:{port}"
@@ -21,10 +27,8 @@ ledger_path = f"{port}/dag.db"
 pr_key=RSA.generate(2048)
 pu_key=pr_key.public_key()
 
-priv_key = pr_key.export_key().decode()
+priv_key = pr_key.export_key()
 pub_key= pu_key.export_key().decode()
-
-
 
 
 os.makedirs(remote_ref,exist_ok=True)
@@ -33,11 +37,22 @@ os.makedirs(remote_obj,exist_ok=True)
 async def send_commit_to_peer(websocket, path):
     global peers
     global port
+    sig_list=[]
     payload = await websocket.recv()
     msg = json.loads(payload)
+
     #print(payload)
     if "ref" in msg:
         ref = msg['ref']
+        #r= SHA256.new(ref.encode())
+        #signature = pkcs1_15.new(pr_key).sign(r)
+        #bla=binascii.hexlify(signature).decode('ascii')
+        #bla2=binascii.unhexlify(bla)
+        #try:
+            #pkcs1_15.new(pu_key).verify(r,bla2)
+            #print("correct verification")
+        #except:
+            #print("unsuccessful verify")
         if not sec.is_valid(msg, ledger_path):
             await websocket.send("Invalid commit")
             print(" NOT VALID")
@@ -49,22 +64,39 @@ async def send_commit_to_peer(websocket, path):
             f.write(msg["body"].encode())
         del msg['ref']
         del msg['body']
+        sig=""
         for key in msg:
             try:
-                db.append_commit(key, msg[key], ledger_path, ref, port)
+                sig = db.append_commit(key, msg[key], ledger_path, ref, pub_key, pr_key)
+                if sig:
+                    signature=binascii.hexlify(sig).decode('ascii')
+                    sig_list.append([key,signature])
             except:
-                print(key)
+                print(key + "Was not successfully added to db")
                 return
             hash=f'{remote_obj}/{key}'
             os.makedirs(os.path.dirname(hash),exist_ok=True )
             with open(hash, 'wb') as f:
                 f.write(msg[key].encode())
 
+
+
     if peers:
+
+        if sig_list !=[]:
+            message= json.loads(payload)
+            print("signatures exist")
+            message["signature"] = sig_list
+            message["from"] = my_path
+
+            payl=json.dumps(message)
+
         for peer in peers:
             async with websockets.connect(peer[0]) as socket:
-                await socket.send(payload)
-                await socket.recv()
+                await socket.send(payl)
+                answer=await socket.recv()
+                if answer:
+                    print(answer)
 
 #connected to by other peers to recive commit, and update peers
 async def recive_commit_from_peer(websocket, path):
@@ -99,22 +131,60 @@ async def recive_commit_from_peer(websocket, path):
 
     elif "ref" in message:
         ref = message['ref']
+        sig_list=[]
+        if "signature" in message:
+            print("sig exist in msg")
+            sig_list=message["signature"]
+            if sig_list==[]:
+                await websocket.send("No signatures for payload")
+                return
+
+            peer_ref= message["from"]
+            p_key=""
+            peer_key=""
+            for peer in peers:
+                if peer[0] == peer_ref:
+                    print("peer found in my peerlist")
+                    peer_key=peer[1]
+                    p_key=RSA.import_key(peer_key)
+                    break
+
+            if peer_key=="":
+                await websocket.send("novalidity")
+                return
+
+
+
+            del message["signature"]
+            del message["from"]
         if not sec.is_valid(message, ledger_path):
             await websocket.send("Invalid commit")
             return
-
         hash=f'{remote_path}/{message["ref"]}'
         os.makedirs(os.path.dirname(hash),exist_ok=True )
         with open(hash, 'wb') as f:
             f.write(message["body"].encode())
         del message['ref']
         del message['body']
+        signature=None
+        sig_keys=[f[0] for f in sig_list]
+
+        #for key in message:
+            #signature=""
+        for sig_pair in sig_list:
+            if sig_pair[0] in message:
+                signature= sig_pair[1]
+                sig= binascii.unhexlify(signature)
+                try:
+                    if signature:
+                        db.get_block(sig_pair[0], message[sig_pair[0]], ledger_path, peer_ref, p_key,peer_key, signature)
+                except:
+                    print("verification process failed")
+                    await websocket.send("no valididity in db")
+                    return
+            else:
+                await websocket.send("sig pair not in message")
         for key in message:
-            try:
-                db.append_commit(key, message[key], ledger_path, ref, port)
-            except:
-                print(key)
-                return
             hash=f'{remote_obj}/{key}'
             os.makedirs(os.path.dirname(hash),exist_ok=True )
             with open(hash, 'wb') as f:
@@ -125,7 +195,7 @@ async def recive_commit_from_peer(websocket, path):
     elif "fetch" in message:
         await websocket.send("OK")
         await send_all_files(message["fetch"])
-
+    await websocket.send("badly formated message")
 # Oatch for sending all files in one json
 # Right now, adding a new peer after a push, it will not get all file data
 async def send_all_files(path):
@@ -197,9 +267,9 @@ async def disconnect():
 if __name__ == '__main__':
     try:
         asyncio.get_event_loop().run_until_complete(fetch_peers())
-        #start_server1 = websockets.serve(send_commit_to_peer,'localhost',2223)
+        start_server1 = websockets.serve(send_commit_to_peer,'localhost',2223)
         start_server2 = websockets.serve(recive_commit_from_peer,'localhost',port)
-        #asyncio.get_event_loop().run_until_complete(start_server1)
+        asyncio.get_event_loop().run_until_complete(start_server1)
         asyncio.get_event_loop().run_until_complete(start_server2)
         asyncio.get_event_loop().run_until_complete(fetch_data())
         asyncio.get_event_loop().run_forever()
